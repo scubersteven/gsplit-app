085ff4ad403b95b7f265ab92bbaf3069f3aab9ec
Justin Shaffer <justinshaffer@Justins-MacBook-Air.local>
2025-12-13 09:41:58 -0500

    Add G logo to wave divider and update header
    - Add glogopro.png to public folder
    - Update WaveDivider with positioned G logo (responsive sizing)
    - Expand wave viewBox to 250 height to accommodate logo
    - Change header background to transparent
    - Logo positioned at left side of wave for both mobile and desktop

--- src/components/WaveDivider.tsx
+++ src/components/WaveDivider.tsx
@@ -2,31 +2,34 @@ import React from 'react';
 
 export function WaveDivider() {
   return (
-    <div className="w-full bg-[#1A1A1A]">
+    <div className="relative w-full">
+      {/* G Logo - positioned on wave */}
+      <img
+        src="/glogopro.png"
+        alt="G"
+        className="absolute z-10 w-[104px] h-[122px] top-[47px] left-[14px] md:w-[140px] md:h-[164px] md:top-[60px] md:left-[40px]"
+      />
+
       {/* Mobile wave */}
       <svg
         className="w-full block md:hidden"
-        viewBox="0 0 393 100"
+        viewBox="0 0 393 250"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="none"
       >
-        <path
-          d="M0 80L9.3 76.5C18.7 73 37.3 68 56 68C74.7 68 93.3 72 112 75C130.7 78 149.3 82 168.2 83C187 84 206 84 224.8 82C243.7 80 262.3 76 281 75C299.7 74 318.3 76 337 77C355.7 78 374.3 77 383.7 75L393 73L393 0L0 0Z"
-          fill="#FFE7AD"
-        />
+        <rect width="393" height="250" fill="#FFE7AD"/>
+        <path d="M0 130L9.3 126.5C18.7 123 37.3 118 56 118C74.7 118 93.3 122 112 125C130.7 128 149.3 132 168.2 133C187 134 206 134 224.8 132C243.7 130 262.3 126 281 125C299.7 124 318.3 126 337 127C355.7 128 374.3 127 383.7 125L393 123L393 250L0 250Z" fill="#1A1A1A"/>
       </svg>
 
       {/* Desktop wave */}
       <svg
         className="w-full hidden md:block"
-        viewBox="0 0 1440 120"
+        viewBox="0 0 1440 250"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="none"
       >
-        <path
-          d="M0 68L40 74.3C80 80.7 160 93.3 240 111.5C320 129.7 400 153.3 480 152.8C560 152.3 640 127.7 720 120.5C800 113.3 880 123.7 960 132.8C1040 142 1120 150 1200 146.3C1280 142.7 1360 127.3 1400 119.7L1440 112L1440 0L0 0Z"
-          fill="#FFE7AD"
-        />
+        <rect width="1440" height="250" fill="#FFE7AD"/>
+        <path d="M0 130L40 126C80 122 160 114 240 118C320 122 400 138 480 142C560 146 640 138 720 130C800 122 880 114 960 118C1040 122 1120 138 1200 142C1280 146 1360 138 1400 134L1440 130L1440 250L0 250Z" fill="#1A1A1A"/>
       </svg>
     </div>
   );
